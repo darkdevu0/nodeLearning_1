02cf31761e9c27d32a0cfe472c22a7150ebbabee
Devashish <Onion4404@gmail.com>
2020-11-12 03:19:58 +0530

    fs

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-const txt = fs.readFileSync('./txt/input.txt', 'utf-8');
+let jsonData, dataObj;
 
-const textOut = `hello \n ${txt} date: ${Date.now()}`;
+jsonData = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+dataObj = JSON.parse(jsonData);
 
-fs.writeFileSync('./txt/final.txt', textOut);
+const server = http.createServer((req, res) => {
+    console.log(req.url);
 
-console.log('File written')
+    const pathName = req.url;
+
+    if (pathName === '/overview') {
+        res.end('Hello from server overview');
+    } else if (pathName === '/product') {
+        res.end('Is product');
+    } else if (pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        })
+        res.end(jsonData);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+        res.end('<h1>home</h1>');
+    }
+
+});
+
+server.listen(3000, () => {
+    console.log('started');
+});
+
+
+                                          
